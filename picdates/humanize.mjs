e0b5f1577e4d165f7c9af8ea76e55e7fa5d623a9
Tomas Fiers <tomas.fiers@gmail.com>
2023-01-24 16:30:49 +0000

    `picdates/`: cleanup (hoist) humanize.mjs

--- picdates/humanize.mjs
+++ picdates/humanize.mjs
@@ -1,25 +1,31 @@
 import dayjs from 'dayjs'
 import { markdownTable } from 'markdown-table'
 
-export const formatDate = (x) => {
-    if (x instanceof dayjs) return x.format("YYYY-MM-DD")
-    return x
-}
-
 /**
  * Convert "myLongString" to "my long string"
  */
 export const camelCaseToSpaced = (str) =>
     str.replace(/([^A-Z])([A-Z])/g, "$1 $2").toLowerCase()
 
-const mapEntries = (obj, {keys = identity, vals = identity} = {}) => {
-    // Rename the functions transforming keys and values, so it's more clear they're
-    // functions
-    const [f, g] = [keys, vals]
-    const transform = ([key, val]) => [f(key), g(val)]
-    return Object.fromEntries(Object.entries(obj).map(transform))
+/**
+ * Given a Dayjs object, format it as an ISO8601 date. Other types are left as is.
+ */
+export const formatDate = (x) => {
+    if (x instanceof dayjs) return x.format("YYYY-MM-DD")
+    return x
 }
-const identity = x => x
+
+/**
+ * Render an array of objects as a markdown table (which is returned as a string)
+ *
+ * The objects are assumed to all have the same keys.
+ * Those keys are used as the table header.
+ */
+export function toMarkdownTable(data) {
+    const header = Object.keys(data[1])
+    const rows = [header, ...data.map(Object.values)]
+    return markdownTable(rows)
+}
 
 export const humanize = (obj) => (
     mapEntries(obj, {
@@ -27,14 +33,18 @@ export const humanize = (obj) => (
         vals: formatDate
     })
 )
-// (can't provide these two funcs as optional arguments:
-//  cannot be used then as `.map(humanize)`)
+// (can't provide the keys and vals funcs as optional arguments:
+//  this func could then not be used as `.map(humanize)`)
 
 /**
- * Render an array of similar objects as a string containing a markdown table
+ * Transform the given object by applying the `keys` function to each of its keys, and
+ * the `vals` function to each of its values.
  */
-export function toMarkdownTable(data) {
-    const header = Object.keys(data[1])
-    const rows = [header, ...data.map(Object.values)]
-    return markdownTable(rows)
+const mapEntries = (obj, {keys = identity, vals = identity} = {}) => {
+    // Rename the two functions that transform keys and values, so that it's more clear
+    // they're functions
+    const [f, g] = [keys, vals]
+    const transform = ([key, val]) => [f(key), g(val)]
+    return Object.fromEntries(Object.entries(obj).map(transform))
 }
+const identity = x => x
