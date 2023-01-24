81143c2b4309d68eae08daeb6c5005fc78a4f4fd
Tomas Fiers <tomas.fiers@gmail.com>
2023-01-24 12:21:25 +0000

    picdates/main.js: polish code

--- picdates/main.js
+++ picdates/main.js
@@ -6,8 +6,7 @@ dayjs.extend(customParseFormat)
 import piexif from 'piexifjs'
 import {markdownTable} from 'markdown-table'
 
-const dir = "/mnt/c/Users/tfiers/Desktop/voorbeeld-fotos-client"
-const filenames = fs.readdirSync(dir)
+const NOTFOUND = "(not found)"
 
 const filenameDatePattern = /\d{8}/
 // All filename dates in client example have this format: yyyymmdd
@@ -15,22 +14,22 @@ const filenameDatePattern = /\d{8}/
 
 const dateFromFilename = (filename) => {
     const result = filenameDatePattern.exec(filename)
-    if (result === null) return null
+    if (result === null) return NOTFOUND
     const extracted = result[0]
     return dayjs(extracted, 'YYYYMMDD')
 }
 
-const readJPEGAsBase64 = path => fs.readFileSync(path).toString('binary')
-const exifOfJPEG = path => piexif.load(readJPEGAsBase64(path))
+const readJPEGAsBase64 = (filepath) => fs.readFileSync(filepath).toString('binary')
+const exifOfJPEG = (filepath) => piexif.load(readJPEGAsBase64(filepath))
 
-const lowercaseExtension = filepath => path.extname(filepath).toLowerCase()
-const isJPEG = filepath => [".jpg", ".jpeg"].includes(lowercaseExtension(filepath))
+const lowercaseExtension = (filepath) => path.extname(filepath).toLowerCase()
+const isJPEG = (filepath) => [".jpg", ".jpeg"].includes(lowercaseExtension(filepath))
 
 const dateFromExif = (filepath) => {
-    if (!isJPEG(filepath)) return null
+    if (!isJPEG(filepath)) return NOTFOUND
     let exif
     try { exif = exifOfJPEG(filepath) }
-    catch (e) { return null }
+    catch (e) { return NOTFOUND }
     const str = exif['Exif'][piexif.ExifIFD.DateTimeOriginal]
     // There's also `exif['0th'][piexif.ImageIFD.DateTime]`
     // but that is either the same; or a later date (date downloaded from camera to PC?)
@@ -46,23 +45,22 @@ const extractDates = (filepath) => {
     }
 }
 
+const dir = "/mnt/c/Users/tfiers/Desktop/voorbeeld-fotos-client"
+const filenames = fs.readdirSync(dir)
 const filepaths = filenames.map(filename => path.join(dir, filename))
 const dateInfo = filepaths.map(extractDates)
 
 const toHumanString = (x) => {
     if (x instanceof dayjs) return x.format("YYYY-MM-DD")
-    if (x === null) return "(not found)"
     return x
 }
-const applyToEntries = (f, obj) => Object.fromEntries(
+const applyToValues = (f, obj) => Object.fromEntries(
     Object.entries(obj).map(([key, val]) => [key, f(val)])
 )
-const humanize = (obj) => applyToEntries(toHumanString, obj)
+const humanize = (obj) => applyToValues(toHumanString, obj)
 const humanDateInfo = dateInfo.map(humanize)
 
 const header = Object.keys(humanDateInfo[0])
 const rows = [header, ...humanDateInfo.map(Object.values)]
 const md = markdownTable(rows)
-// json = JSON.stringify(humanDateInfo, null, 4)
-// fs.writeFileSync("picdates/output.json", json)
 fs.writeFileSync("picdates/output.md", md)
