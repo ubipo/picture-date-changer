2b64a90e2e845cc5640552178e435d712c3101b1
Tomas Fiers <tomas.fiers@gmail.com>
2023-01-24 13:32:10 +0000

    compare extracted w/ ground-truth labels
    also:
    - fix one wrong date in client-labels.js (this files is also renamed, from labels_client.js)
    - update readme (one task done)
    - nicer table header: "lower sentence case" instead of "camelCase"

--- picdates/main.js
+++ picdates/main.js
@@ -5,6 +5,7 @@ import customParseFormat from 'dayjs/plugin/customParseFormat.js'
 dayjs.extend(customParseFormat)
 import piexif from 'piexifjs'
 import {markdownTable} from 'markdown-table'
+import {clientLabels} from './client-labels.js'
 
 const NOTFOUND = "(not found)"
 
@@ -39,28 +40,45 @@ const dateFromExif = (filepath) => {
 const extractDates = (filepath) => {
     const filename = path.basename(filepath)  // (includes extension, but that's ok)
     return {
-        filename: filename,
         dateFromFilename: dateFromFilename(filename),
         dateFromExif: dateFromExif(filepath),
     }
 }
 
-const dir = "/mnt/c/Users/tfiers/Desktop/voorbeeld-fotos-client"
-const filenames = fs.readdirSync(dir)
-const filepaths = filenames.map(filename => path.join(dir, filename))
-const dateInfo = filepaths.map(extractDates)
+const trueDates = {}
+clientLabels.forEach(day => {
+    day['filenames'].forEach(filename => {
+        trueDates[filename] = dayjs(day['date'], "YYYY-MM-DD")
+    })
+})
+const labelledFilenames = Object.keys(trueDates)
 
-const toHumanString = (x) => {
+const pictureDir = "/mnt/c/Users/tfiers/Desktop/voorbeeld-fotos-client"
+const toPath = (filename) => path.join(pictureDir, filename)
+
+const fileDates = labelledFilenames.map(filename => ({
+    filename,
+    trueDate: trueDates[filename],
+    ...extractDates(toPath(filename)),
+}))
+const compareBy = (f) => ((a, b) => f(a) - f(b))
+fileDates.sort(compareBy(obj => obj['trueDate'].unix()))
+
+const formatDate = (x) => {
     if (x instanceof dayjs) return x.format("YYYY-MM-DD")
     return x
 }
-const applyToValues = (f, obj) => Object.fromEntries(
-    Object.entries(obj).map(([key, val]) => [key, f(val)])
-)
-const humanize = (obj) => applyToValues(toHumanString, obj)
-const humanDateInfo = dateInfo.map(humanize)
+const camelCaseToSpaced = (str) => str.replace(/([^A-Z])([A-Z])/g, "$1 $2").toLowerCase()
 
-const header = Object.keys(humanDateInfo[0])
-const rows = [header, ...humanDateInfo.map(Object.values)]
+const identity = x => x
+const mapEntries = (obj, {keys = identity, vals = identity} = {}) => Object.fromEntries(
+    Object.entries(obj).map(([key, val]) => [keys(key), vals(val)])
+)
+
+const humanize = (obj) => mapEntries(obj, {keys: camelCaseToSpaced, vals: formatDate})
+const humanFileDates = fileDates.map(humanize)
+
+const header = Object.keys(humanFileDates[0])
+const rows = [header, ...humanFileDates.map(Object.values)]
 const md = markdownTable(rows)
 fs.writeFileSync("picdates/output.md", md)
